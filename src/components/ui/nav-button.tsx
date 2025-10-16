b3a826a5d81177b218c2fa88c3ce6e3862aca058
Thiago Andrade <tja.usa.202@gmail.com>
2025-10-16 13:49:37 -0400

    Make text centered

--- src/components/ui/nav-button.tsx
+++ src/components/ui/nav-button.tsx
@@ -12,7 +12,7 @@ export const NavButton = ({
     <button
       onClick={onClick}
       className="p-2 min-w-[100px] border-2 
-      cursor-pointer flex justify-center bg-blue-600 text-white font-bold 
+      cursor-pointer flex justify-center items-center bg-blue-600 text-white font-bold 
       border-t-blue-400 border-l-blue-400 border-b-blue-700 
       border-r-blue-700 hover:bg-blue-700"
     >
