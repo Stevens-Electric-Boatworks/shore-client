bfcd2e7bca8a7c80706efca6f6d1ad6b39c4e180
Thiago Andrade <tja.usa.202@gmail.com>
2026-02-28 14:29:01 -0500

    Remove unfinished terminal component

--- src/app/diag/terminal/page.tsx
+++ src/app/diag/terminal/page.tsx
@@ -1,11 +1,8 @@
-import { Terminal } from "@/components/terminal";
-
 export default function TerminalPage() {
   return (
     <div className="flex flex-col h-full">
       <title>Terminal</title>
       <p className="text-xl font-bold mb-4">Terminal</p>
-      <Terminal />
     </div>
   );
 }
