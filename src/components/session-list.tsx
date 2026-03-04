5c6325adb6ab3618f3eb439044b3b7fdd74707e0
Thiago Andrade <tja.usa.202@gmail.com>
2026-03-04 13:46:33 -0500

    Added session list

--- src/components/session-list.tsx
+++ src/components/session-list.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import axios from "axios";
+import { LineSpinner } from "ldrs/react";
 import { useEffect, useState } from "react";
+import { NavButton } from "./ui/nav-button";
 
 type SessionEntry = {
   id: number;
@@ -9,26 +11,39 @@ type SessionEntry = {
   endTime?: Date;
 };
 
-type Pagination = {
-  cursor?: string; // ID of the last item from previous page
-  limit?: string;
-  order?: "asc" | "desc";
+type PaginationResponse = {
+  nextCursor?: number;
+  hasNextPage: boolean;
+  limit: number;
 };
 
 export const SessionList = () => {
-  const [data, setData] = useState<SessionEntry | undefined>(undefined);
+  const [data, setData] = useState<SessionEntry[] | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
-  const [pagination, setPagination] = useState<Pagination | undefined>(
+  const [pagination, setPagination] = useState<PaginationResponse | undefined>(
     undefined,
   );
 
-  const retrieveData = async () => {
+  const retrieveData = async (cursor?: number) => {
     try {
       setIsLoading(true);
       const res = await axios.get(
         "https://shore.stevenseboat.org/api/sessions",
+        { params: { cursor } },
       );
+      setData((s) => {
+        const newData: SessionEntry[] = res.data.data.map(
+          (e: SessionEntry) => ({
+            id: e.id,
+            startTime: new Date(e.startTime),
+            endTime: e.endTime ? new Date(e.endTime) : undefined,
+          }),
+        );
+        if (cursor && s) return [...s, ...newData].slice(-1000);
+        else return newData;
+      });
+      setPagination(res.data.pagination);
     } catch (err) {
       setError(err as string);
     } finally {
@@ -41,19 +56,47 @@ export const SessionList = () => {
   }, []);
 
   return (
-    <div className="bg-white flex-1 h-full border">
-      <table className="w-full">
-        <colgroup>
-          <col span={1} width={60} />
-        </colgroup>
-        <thead className="sticky top-0 bg-gradient-to-b from-zinc-100 to-zinc-300 text-left border-b">
-          <tr>
-            <th className="px-2 border-b font-normal">ID</th>
-            <th className="border-b font-normal">Begin</th>
-            <th className="border-b font-normal">End</th>
-          </tr>
-        </thead>
-      </table>
+    <div className="bg-white border flex flex-col h-full">
+      <div className="p-2 flex items-center bg-gradient-to-b from-zinc-100 to-zinc-300 border-b shrink-0">
+        <p className="font-bold">Session List</p>
+        <div className="ml-auto flex gap-2 items-center">
+          {isLoading && <LineSpinner size={20} stroke={1.5} color="#333" />}
+          <NavButton size="small" onClick={() => retrieveData(undefined)}>
+            REFRESH
+          </NavButton>
+        </div>
+      </div>
+      <div className="min-h-0 flex-1 overflow-y-auto flex flex-col">
+        <table className="">
+          <colgroup>
+            <col span={1} width={60} />
+          </colgroup>
+          <thead className="sticky top-0 bg-gradient-to-b from-zinc-100 to-zinc-300 text-left border-b">
+            <tr>
+              <th className="px-2 border-b font-normal">ID</th>
+              <th className="border-b font-normal">Begin</th>
+              <th className="border-b font-normal">End</th>
+            </tr>
+          </thead>
+          <tbody className="[&>tr:nth-child(even)]:bg-gray-100 [&>tr>td:first-child]:font-bold [&>tr>td:first-child]:text-center">
+            {data?.map((e) => (
+              <tr key={e.id}>
+                <td>{e.id}</td>
+                <td>{e.startTime.toLocaleString()}</td>
+                <td>{e.endTime ? e.endTime?.toLocaleString() : ""}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {pagination?.hasNextPage && (
+          <button
+            className="w-full text-center text-sm font-bold py-2 hover:cursor-pointer hover:bg-zinc-100 underline text-blue-600 hover:text-blue-800"
+            onClick={() => retrieveData(pagination?.nextCursor)}
+          >
+            Load more results
+          </button>
+        )}
+      </div>
     </div>
   );
 };
